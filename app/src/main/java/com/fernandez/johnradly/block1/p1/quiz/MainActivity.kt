8fe88c1bddde3b8f5a7374d84eab3c284ea456c6
shakia <shma.javier.up@phinmaed.com>
2023-12-09 19:01:05 +0800

    2

--- app/src/main/java/com/fernandez/johnradly/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/fernandez/johnradly/block1/p1/quiz/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.fernandez.johnradly.block1.p1.quiz
+package com.fernandez.johnradly.block2.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fernandez.johnradly.block1.p1.quiz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        println("Hello world!")
     }
 }
