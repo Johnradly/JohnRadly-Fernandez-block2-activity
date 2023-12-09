e39c2d6cf8347b7ac63054fe0e1973c022fb3b72
shakia <shma.javier.up@phinmaed.com>
2023-12-09 15:07:11 +0800

    1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JohnRadly-Fernandez-Block1-Quiz"
+include(":app")
+ 
